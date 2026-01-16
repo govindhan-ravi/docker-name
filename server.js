4572f385b27d7b5215bae587b5b85f2e47ba9941
govindhan-ravi <govindhanr7867@gmail.com>
2026-01-16 12:52:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('govindhan\narun\nmoinca');
+  res.end('govindhan\narun\nmoinca\nramya');
 }).listen(8080, '0.0.0.0');
